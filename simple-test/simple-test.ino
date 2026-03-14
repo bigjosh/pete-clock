0ce2530db22264cad190502c5688d396bc007b2d
josh <github@junk.josh.com>
2026-03-14 00:38:48 -0400

    Simplest working code POC

---
+++ simple-test/simple-test.ino
@@ -0,0 +1,97 @@
+
+// see "driving puls ein digital mode" in the datasheet
+// Sequncing from the VID28-05 dtatasheet using thier names
+
+// It takes 6 phase per step
+#define PHASE_COUNT 6
+
+// There are 4 pins per axle. The datsheet calls them CONATC1 - CONATCT4. 
+#define CONTACT_COUNT 4
+
+
+// We execute these steps on the output pins in order to turn
+// again taken directly from the datasheet, even using thier names
+// Note that Contacts 2 and 3 are always the same. The datasheets combines them into a single trace.
+const int phases[PHASE_COUNT][CONTACT_COUNT] = {
+  {1,0,0,1},    //1
+  {0,0,0,1},    //2
+  {0,1,1,1},    //3
+  {0,1,1,0},    //4
+  {1,1,1,0},    //5
+  {1,0,0,0},    //6
+};
+
+
+// Pins from VID28 datasheet . Also called contacts.
+// First is the pin name in the datasheet, next is the color of our wire, last is the Arduino IO pin it is connected to
+// Inner motor - 
+//  A1 = BLUE = 5
+//  A2 = PURPLE = 4
+//  A3 = YELLOW = 7
+//  A4 = GREEN = 6
+
+// B1 - RED - 9
+// B2 - ORANGE - 8
+// B3 - GREY - 3
+// B4 - WHITE 2
+
+#define INNER_AXLE   0
+#define OUTER_AXLE  1
+
+const int motorPins[2][CONTACT_COUNT] = {
+  {5, 4, 7, 6},   // Inner axle  
+//  {4, 7, 6, 5},   // Inner axle
+  {9, 8, 3, 2},   // Outer axle
+};
+
+
+// Variables to track where each motor is in the sequence
+int inner_phase = 0; 
+int outer_phase = 0;
+int outer_phase_substep =0;
+
+void setup() {
+  // Set all motor pins as outputs
+  for (int i = 0; i < CONTACT_COUNT; i++) {
+    pinMode(motorPins[INNER_AXLE][i], OUTPUT);
+    pinMode(motorPins[OUTER_AXLE][i], OUTPUT);
+  }
+}
+
+void loop() {
+  
+  // Apply the current phase to the pins
+  for (int i = 0; i < CONTACT_COUNT; i++) {    
+    digitalWrite(motorPins[INNER_AXLE][i], phases[inner_phase][i]);
+    digitalWrite(motorPins[OUTER_AXLE][i], phases[outer_phase][i]);
+  }
+
+  // Note that the two axles seem to be geared in opposite directions. 
+  // Also note that the outer axle seems to need more torque. 
+
+
+  // outer axle backward (Clockwise)
+  inner_phase++;
+  if (inner_phase >=5 ) {
+    inner_phase = 0;
+  } 
+
+  // Run the outer axel at 1/2 speed becuase (1) it needs more torque and (2) it looks cooler.
+
+  outer_phase_substep = !outer_phase_substep;
+
+  if (outer_phase_substep) {
+    
+ 
+    // Outter axle forward (Counter - Clockwise)
+    outer_phase++;
+    if (outer_phase >= 5) {
+      outer_phase = 0; // Loop back to start
+    }
+
+  }
+  
+
+  // longer delay = slower spin
+  delay(1); 
+}
